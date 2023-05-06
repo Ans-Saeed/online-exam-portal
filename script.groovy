05d6c5b707a7f61e84d031e0093641fa83a0f868
Ans <anssaeed.pk@gmail.com>
2023-05-06 17:46:09 +0500

    added jenkins file

---
+++ script.groovy
@@ -0,0 +1,35 @@
+def buildServer(){
+    withCredentials([
+        usernamePassword(credentialsId:'dockerhub-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')
+    ]){
+        // sh 'cd server/'
+        sh 'docker build -t anssaeed/my-repo:server1.0  /var/jenkins_home/workspace/online-exam-portal-pipeline/server/'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'docker push anssaeed/my-repo:server1.0'
+    }
+
+}
+def buildFrontend(){
+    withCredentials([
+        usernamePassword(credentialsId:'dockerhub-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')
+    ]){
+        // sh 'cd frontend/'
+        sh 'docker build -t anssaeed/my-repo:frontend1.0  /var/jenkins_home/workspace/online-exam-portal-pipeline/frontend/'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'docker push anssaeed/my-repo:frontend1.0'
+    }
+
+}
+def buildUserPortal(){
+    withCredentials([
+        usernamePassword(credentialsId:'dockerhub-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')
+    ]){
+        // sh 'cd user-portal-frontend/'
+        sh 'docker build -t anssaeed/my-repo:userportal1.0  /var/jenkins_home/workspace/online-exam-portal-pipeline/user-portal-frontend/'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'docker push anssaeed/my-repo:userportal1.0'
+    }
+
+}
+
+return this
